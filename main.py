b1e18f25a81ef51b88394e7df9a874df4325cd73
wenxi ye <wye001@uoc.edu>
2024-04-13 22:05:34 +0200

    añado requirements y mas dataset

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from source.scrapers.pharma import DrugsScraper
 # Example usage
 scraper = DrugsScraper()
-scraper.scrape_drugs_by_year(years=[2018])
+scraper.scrape_drugs_by_year(years=[2002])
 scraper.fetch_specifics()
 scraper.get_review_data()
-scraper.save_to_csv(file_name="farmacos_2018_no_reviews.csv")
+scraper.save_to_csv(file_name="farmacos_2002.csv")
